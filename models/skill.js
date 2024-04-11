492b95f3addb6bfebb8ee4faa4359d32b1a06f2c
Qilian(Daisy) <qiliansong@gmail.com>
2024-04-11 16:48:01 -0700

    On the show view, display a Delete Skill button (the submit button within a <form>) that when clicked, deletes the skill from the “database” and redirects to the index view

--- models/skill.js
+++ models/skill.js
@@ -4,11 +4,30 @@ const skills = [
     {id: 3, skill: 'HTML',level: 'Advanced'},
     {id: 4, skill: 'CSS',level: 'Advanced'}
   ];
-	
+
   module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne
   };
+
+  function deleteOne(id) {
+    // All properties attached to req.params are strings!
+    id = parseInt(id);
+    // Find the index based on the id of the todo object
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+  }
+
+  function create(skill) {
+    // Add the id
+    skill.id = Date.now() % 1000000;
+    // New todos wouldn't be done :)
+    skill.level = 'beginner';
+    console.log(skill);
+    skills.push(skill);
+  }
 
   function getOne(id) {
     // URL params are strings - convert to a number
--- routes/skills.js
+++ routes/skills.js
@@ -8,6 +8,10 @@ router.get('/', skillsCtrl.index);
 router.get('/new', skillsCtrl.new);
 
 router.get('/:id', skillsCtrl.show);
+//POST /skills
+router.post('/', skillsCtrl.create); 
+//DELETE /skills/:id
+router.delete('/:id', skillsCtrl.delete);
 
 
 module.exports = router;
